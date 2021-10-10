9e81c8f73cb2558dbc3d1c774385c198151576e4
saulatgondal <saulatgondal1@gmail.com>
2021-10-11 02:52:22 +0500

    Added the functionality in subject_no_selection class for generating the fields as per the user enters the number of courses and labs in popActivity class.
    Only user specified fields would be generated.
    
    Also added the linkage between the popActivity and subject_no_selection on clicking the next button.
    
    Now, a University name on a popActivity would be shown based on which university card view is clicked.

--- app/src/main/java/com/hunt4bit/gpacalculaterpk/popActivity.java
+++ app/src/main/java/com/hunt4bit/gpacalculaterpk/popActivity.java
@@ -2,10 +2,18 @@ package com.hunt4bit.gpacalculaterpk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.IntentSender;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EdgeEffect;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class popActivity extends AppCompatActivity {
 
@@ -13,6 +21,25 @@ public class popActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pop);
+        TextView heading= findViewById(R.id.textView6);
+        Button nextBut = findViewById(R.id.NextBut);
+
+        EditText NOfcourses = findViewById(R.id.NocTextF);
+        EditText NOfLabs = findViewById(R.id.NolTextF);
+
+
+
+
+
+        //Setting title of the popup window
+        Intent intent =getIntent();
+        String uniName= intent.getStringExtra("uniName");
+        heading.setText(uniName.toUpperCase());
+
+
+        //Sending the number of courses and labs to subject_no_selection class
+        //to generate the number of subjects and labs accordingly
+
 
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
@@ -21,7 +48,28 @@ public class popActivity extends AppCompatActivity {
         int width = dm.widthPixels;
         int height = dm.heightPixels;
 
-        getWindow().setLayout((int) (width *.8),(int) (height*.3));
+        getWindow().setLayout((int) (width *.8),(int) (height*.4));
         getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+
+        nextBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intents =new Intent(popActivity.this, subject_no_selection.class);
+
+
+                Bundle courseExtras= new Bundle();
+                courseExtras.putString("courses", NOfcourses.getText().toString());
+
+                Bundle labExtras= new Bundle();
+                labExtras.putString("labs", NOfLabs.getText().toString());
+
+
+                intents.putExtras(courseExtras);
+                intents.putExtras(labExtras);
+                startActivity(intents);
+
+            }
+        });
     }
 }
